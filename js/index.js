568c85b7cfe8ed00d81d6d5dd96b634c64998a90
Saul Acosta <saulmosso926@gmail.com>
2025-09-23 19:40:28 -0700

    js added, created footer and skills

---
+++ js/index.js
@@ -0,0 +1,23 @@
+
+const body = document.querySelector('body');
+const footer = document.createElement('footer');
+body.appendChild(footer);
+
+const today = new Date();
+const thisYear = today.getFullYear();
+
+const copyright = document.createElement('p');
+copyright.innerHTML = `© Saul Acosta ${thisYear}`;
+
+footer.appendChild(copyright);
+
+const skills = ["HTML", "CSS", "JavaScript", "Git", "GitHub", "Responsive Design", "Web Development", "Code the Dream"];
+
+const skillsSection = document.getElementById('skills');
+const skillsList = skillsSection.querySelector('ul');
+
+for (let i = 0; i < skills.length; i++) {
+    const skills = document.createElement('li');
+    skills.innerText = skills[i];
+    skillsList.appendChild(skills);
+}
